d920010337137bdc5c72ff9012a4a4908c46ca55
shilingas <mariussilingas5@gmail.com>
2024-04-19 10:11:07 +0300

    backend structure, removed unnecessary folders

--- FitnessTrackingApp/ClientApp/src/app/app.module.ts
+++ FitnessTrackingApp/ClientApp/src/app/app.module.ts
@@ -5,18 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
