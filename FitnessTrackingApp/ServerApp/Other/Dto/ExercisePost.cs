1457e9b61234452b11207c611b67d500bfe92fce
Gstsjk <juskevicius.gustas@gmail.com>
2024-05-05 18:01:24 +0300

    fixed post dto

--- FitnessTrackingApp/ServerApp/Other/Dto/ExercisePost.cs
+++ FitnessTrackingApp/ServerApp/Other/Dto/ExercisePost.cs
@@ -1,7 +1,10 @@
-﻿namespace FitnessTrackingApp.ServerApp.Other.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessTrackingApp.ServerApp.Other.Dto
 {
     public class ExercisePost
     {
+        [Required]
         public string Title { get; set; }
 
         public string? Description { get; set; }
